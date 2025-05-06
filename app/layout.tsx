39304178d98d36b061bcfaefef0fff1c2df2eb2e
zxcsix-zxc <terranoiz@icloud.com>
2025-05-06 20:39:35 +0300

    deleted ads related code

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,9 +14,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9522089510728070" crossOrigin="anonymous"></script>
-      </head>
       <body className={inter.className}>
         <LanguageProvider>
           {children}
